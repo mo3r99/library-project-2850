12c76c502c18334e0405bb059654fe400d4b5496
Muhammad Rauf <muhammad.s.rauf@proton.me>
2026-02-18 15:26:02 +0000

    Renamed HTMX

--- server/src/main/kotlin/common/LayoutTemplate.kt
+++ server/src/main/kotlin/common/LayoutTemplate.kt
@@ -15,7 +15,7 @@ class LayoutTemplate : Template<HTML> {
             meta(name = "viewport", content = "width=device-width, initial-scale=1")
             meta(name = "color-scheme", content = "light dark")
             link(rel = "stylesheet", href = "https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.jade.min.css")
-            scriptLink(href = "/htmx2-0-8.js", type = "text/javascript")
+            script(src = "/htmx/htmx2_0_8.js", type = ScriptType.textJavaScript) {}
 
             title {
                 insert(titleText)
