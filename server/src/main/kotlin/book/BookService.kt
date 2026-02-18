32366d55a5ed7d9c29883bf24d7257554d07c066
Muhammad Rauf <muhammad.s.rauf@proton.me>
2026-02-18 15:25:35 +0000

    Add author

--- server/src/main/kotlin/book/BookService.kt
+++ server/src/main/kotlin/book/BookService.kt
@@ -1,14 +1,16 @@
 package book
 
 import author.Author
+import author.AuthorTable
+import org.jetbrains.exposed.v1.core.eq
 import org.jetbrains.exposed.v1.jdbc.transactions.suspendTransaction
 
 class BookService {
-    suspend fun getAllBooks(): Map<Book, Author> {
-        var books: MutableMap<Book,Author> = mutableMapOf<Book, Author>()
+    suspend fun getAllBooksWithAuthors(): Map<Book, String> {
+        var books: MutableMap<Book, String> = mutableMapOf<Book, String>()
         suspendTransaction {
             val allBooks = Book.all().forEach { book ->
-                books[book] = book.author
+                books[book] = book.author.name
             }
         }
         return books
