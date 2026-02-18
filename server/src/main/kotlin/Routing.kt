7d401c5dd31f5259cc1c74f7bf2b8b7cb2a83636
Muhammad Rauf <muhammad.s.rauf@proton.me>
2026-02-18 15:26:57 +0000

    Refactored home page

--- server/src/main/kotlin/Routing.kt
+++ server/src/main/kotlin/Routing.kt
@@ -1,50 +1,14 @@
 package leeds.compsci
 
-import book.BookService
-import common.LayoutTemplate
-import io.ktor.server.application.Application
-import io.ktor.server.http.content.staticResources
-import io.ktor.server.routing.get
-import io.ktor.server.routing.routing
-import kotlinx.html.h1
-import kotlinx.html.td
-import kotlinx.html.tr
-import kotlinx.html.table
-import io.ktor.server.html.respondHtmlTemplate
-import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.routing.*
+import leeds.compsci.routes.homeRoute
 
-suspend fun Application.configureRouting() {
-    val bookService = BookService()
-    val books = bookService.getAllBooks()
-
+fun Application.configureRouting() {
     routing {
-        staticResources("/", "/web")
+        staticResources("/", "content")
 
-        get("/") {
-            call.respondHtmlTemplate(LayoutTemplate()) {
-                titleText { +"Library" }
-
-                content {
-                    h1 { +"Welcome to the Library" }
-                    table {
-                        tr {
-                            td { +"Title" }
-                            td { +"Isbn"}
-                            td { +"Author" }
-                        }
-
-                        books.forEach { book ->
-                            tr {
-                                td { +book.key.title }
-                                td { if (book.key.isbn == "0") +"N/A" else +book.key.isbn }
-                                transaction {
-                                    td { +book.value.name }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        get("/") { call.homeRoute() }
     }
 }
