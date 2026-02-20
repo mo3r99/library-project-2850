bc2427a15433f454d44666a70c7b1945bd233e26
Muhammad Rauf <muhammad.s.rauf@proton.me>
2026-02-20 16:31:40 +0000

    Added search functionality

--- server/src/main/kotlin/Routing.kt
+++ server/src/main/kotlin/Routing.kt
@@ -4,11 +4,13 @@ import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
 import leeds.compsci.routes.homeRoute
+import leeds.compsci.routes.searchRoute
 
 fun Application.configureRouting() {
     routing {
         staticResources("/", "content")
 
         get("/") { call.homeRoute() }
+        get("/search") { call.searchRoute() }
     }
 }
