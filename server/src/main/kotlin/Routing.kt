4b39b1827d90cd726414b61f7e7f0b833318123a
Muhammad Rauf <muhammad.s.rauf@proton.me>
2026-02-21 16:01:36 +0000

    Add route for reserving books

--- server/src/main/kotlin/Routing.kt
+++ server/src/main/kotlin/Routing.kt
@@ -3,9 +3,11 @@ package leeds.compsci
 import io.ktor.server.application.Application
 import io.ktor.server.http.content.staticResources
 import io.ktor.server.routing.get
+import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
 import leeds.compsci.routes.bookDetailRoute
 import leeds.compsci.routes.homeRoute
+import leeds.compsci.routes.reserveRoute
 import leeds.compsci.routes.searchRoute
 
 fun Application.configureRouting() {
@@ -15,5 +17,7 @@ fun Application.configureRouting() {
         get("/") { call.homeRoute() }
         get("/search") { call.searchRoute() }
         get("/book/{id}") { call.bookDetailRoute() }
+
+        post("/reserve/{bookId}/{copyId}") { call.reserveRoute() }
     }
 }
